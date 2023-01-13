7bec16231aa3c3833fb69f4f4ea29960107e644f
RoyZ <zroy2000@gmail.com>
2023-01-13 09:16:48 +0200

    Modification

--- FactoryProject/Models/UsersBL.cs
+++ FactoryProject/Models/UsersBL.cs
@@ -17,10 +17,23 @@ namespace FactoryProject.Models
 			return _context.Users.ToList();
 		}
 
-        
-
-    
-		
+        public bool LogInUser(string UserName, string Password) 
+        {
+            bool coin = false;
+            var CheckUser = _context.Users.Where(user => user.userName == UserName && user.password == Password).First();
+            if(CheckUser == null) 
+            {
+                return coin = false;
+            }
+            else if(CheckUser != null) 
+            {
+                return coin = true;
+            }
+            else
+            {
+                return coin = false;
+            }
+        }
 	}
 }
 
