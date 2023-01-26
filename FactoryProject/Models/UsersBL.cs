c50859ed858788e8c3c05ad7c35d003e16738363
RoyZ <zroy2000@gmail.com>
2023-01-26 18:06:23 +0200

    Revert "update"
    This reverts commit 02c9df7aac4679423fefc5da7b32a4a3ca29b11c.

--- FactoryProject/Models/UsersBL.cs
+++ FactoryProject/Models/UsersBL.cs
@@ -28,7 +28,8 @@ namespace FactoryProject.Models
                 return _auth = false;
             }
             else if(CheckUser != null) 
-            {                
+            {
+                UserRequests();
                 return _auth = true;
             }
             else
@@ -41,7 +42,13 @@ namespace FactoryProject.Models
             return _auth = false;
         }
 
-     
+        public int UserRequests () {
+            var LoggedUser = _context.Users.Where(user => user.id == _userId).First();
+            LoggedUser.numOfActions--;
+            _context.SaveChanges();
+
+            return LoggedUser.numOfActions;
+        }
 
 	}
 }
